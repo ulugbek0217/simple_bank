edd20881927e092a9b6def58ea00b334d1d51852
Ulugbek <mylogin.ulugbek@gmail.com>
2025-02-13 13:42:55 +0500

    tests for crud

--- util/random.go
+++ util/random.go
@@ -12,6 +12,7 @@ func RandomInt(min, max int64) int64 {
 	return rand.Int63n(max-min+1) + min
 }
 
+// RandomString generated a random string with n length
 func RandomString(n int) string {
 	var sb strings.Builder
 	k := len(alphabet)
@@ -23,14 +24,17 @@ func RandomString(n int) string {
 	return sb.String()
 }
 
+// RandomOwner generates a random owner with length of 6
 func RandomOwner() string {
 	return RandomString(6)
 }
 
+// RandomMoney generates random amount of money between 0 and 1000
 func RandomMoney() int64 {
 	return RandomInt(0, 1000)
 }
 
+// RandomCurrency returns an exact currency between 3
 func RandomCurrency() string {
 	currencies := []string{"USD", "EUR", "CAD"}
 	n := len(currencies)
